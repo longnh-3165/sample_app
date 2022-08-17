7f5d970b79b91b1b29764984381d8e2dcb855e06
ngo.hoang.long <SUN-ASTERISK\ngo.hoang.long@b123165-pc.sun-asterisk.com>
2022-08-17 13:27:10 +0700

    Chaper 6: Modeling User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   validates :name, presence: true,
             length: {maximum: Settings.digits.length_name_max_50}
   validates :email, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    get "static_pages/home"
-    get "static_pages/help"
+    root "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
